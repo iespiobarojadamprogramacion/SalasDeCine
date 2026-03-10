9c32ce64e6d47861619c1037c973cd0ff5155f9a
Rafael Antonio Barahona Dubon <rafael.barahona@educa.madrid.org>
2026-03-10 20:22:26 +0100

    .

--- SalasDeCine/src/salasdecine/Principal.java
+++ SalasDeCine/src/salasdecine/Principal.java
@@ -5,14 +5,13 @@ public class Principal {
 	public static void main(String[] args) {
 
 	
-
 		System.out.println("Soy Jesus");
 
 		System.out.println("Rodrigop");
 
 		System.out.println("Soy Rafael");
 
-    System.out.println("Hola soy Jorge");	
+		System.out.println("Hola soy Jorge");	
     
 	}
 
