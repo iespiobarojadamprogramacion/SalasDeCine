3d04e3f4af5347908cd785a7ee64cacf58ced64f
rodrigopal2 <rodrigo.palomo2@educa.madrid.org>
2026-03-11 19:56:00 +0100

    act2 Clientes

---
+++ SalasDeCine/src/salasdecine/modelo/Cliente.java
@@ -0,0 +1,46 @@
+package salasdecine.modelo;
+
+public class Cliente {
+	private static int contador=0;
+	private int id_Cliente;
+	private String Nombre;
+	private String Email;
+	private String fecha_nacimiento;
+	public Cliente(String nombre, String email, String fecha_nacimiento) {
+		this.id_Cliente=++contador;
+		Nombre = nombre;
+		Email = email;
+		this.fecha_nacimiento = fecha_nacimiento;
+	}
+	public static int getContador() {
+		return contador;
+	}
+	public static void setContador(int contador) {
+		Cliente.contador = contador;
+	}
+	public int getId_Cliente() {
+		return id_Cliente;
+	}
+	public void setId_Cliente(int id_Cliente) {
+		this.id_Cliente = id_Cliente;
+	}
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getFecha_nacimiento() {
+		return fecha_nacimiento;
+	}
+	public void setFecha_nacimiento(String fecha_nacimiento) {
+		this.fecha_nacimiento = fecha_nacimiento;
+	}
+	
+}
